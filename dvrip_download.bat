29490f56e340f1c26bcbca441d0bca4d0f7898a1
chshkhr <chshkhr@gmail.com>
2022-07-26 15:10:07 +0300

    start=now

--- dvrip_download.bat
+++ dvrip_download.bat
@@ -1,3 +1,3 @@
 set fn=%1
-rem 226-20220726-1020                                                                                               
+set fn=%fn:"=%
 dvrip_download.exe 192.168.108.%fn:~0,3% vlad vlad37650 "%fn:~10,2%.%fn:~8,2%.%fn:~6,2% %fn:~13,2%:%fn:~15,2%" 0

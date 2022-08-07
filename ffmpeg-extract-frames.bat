f205d2183b09e21381998297885a4798830bbca2
chshkhr <chshkhr@gmail.com>
2022-08-07 11:13:21 +0300

    more correct extraction 1 fr/sec

--- ffmpeg-extract-frames.bat
+++ ffmpeg-extract-frames.bat
@@ -2,4 +2,5 @@ set fn=%1
 set fnne=%~n1
 set ext=%~x1
 mkdir %fnne%
-ffmpeg -i %fn% -vf "select=not(mod(n\,10))" -vsync vfr %fnne%\frame-%%03d.jpg
+rem ffmpeg -i %fn% -vf "select=not(mod(n\,10))" -vsync vfr %fnne%\frame-%%03d.jpg
+ffmpeg -i %fn% -r 1 -f image2 %fnne%\frame-%%03d.jpg
